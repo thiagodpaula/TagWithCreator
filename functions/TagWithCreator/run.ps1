3700a6c466f54a654f1ef29a2aca569c9a924857
Thiago Paula <thiago.depaula@outlook.com>
2022-09-23 14:38:47 -0300

    Update verion 3 to 4

--- functions/TagWithCreator/run.ps1
+++ functions/TagWithCreator/run.ps1
@@ -1,4 +1,38 @@
+<#
+.SYNOPSIS
+  This Azure Function is used to tag new resource groups with the user name of the creator.
+.DESCRIPTION
+  ****This script provided as-is with no warranty. Test it before you trust it.****
+  Event Grid send a trigger to the Azure Function when a resource group is added to a subscription.  
+  Advanced filters in Event Grid limit the alter to a defined data type.  
+  Resource types are specified in the input data from Event Grid.  Resource Provider Operations are listed in the document:
+  https://docs.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations#microsoftcompute
+  The Azure function formats the tag and value from the Event Grid data.
+  Azure function applies the tag and tag value to the resource group.
+.INPUTS
+  Data passed in from Event Grid
+.OUTPUTS
+  Errors write to the Error stream
+  Logging and test data writes to the Output stream
+.NOTES
+  Version:        1.0
+  Author:         Travis Roberts
+  Creation Date:  4/27/2021
+  Purpose/Change: Initial script development
+  ****This script provided as-is with no warranty. Test it before you trust it.****
+.EXAMPLE
+  TBD
+#>
+
+
+# Parameter Name must match bindings
 param($eventGridEvent, $TriggerMetadata)
+
+# Get the day in Month Day Year format
+$date = Get-Date -Format "MM/dd/yyyy"
+# Add tag and value to the resource group
+$nameValue = $eventGridEvent.data.claims.name
+$tagsupdate = @{"Creator"="$nameValue";"DateCreated"="$date"}
 
 $caller = $eventGridEvent.data.claims.name
 if ($null -eq $caller) {
@@ -10,11 +44,19 @@ if ($null -eq $caller) {
         }
     }
 }
-Write-Host "Caller: $caller"
-$resourceId = $eventGridEvent.data.resourceUri
-Write-Host "ResourceId: $resourceId"
 
-if (($null -eq $caller) -or ($null -eq $resourceId)) {
+write-output "Tags:"
+write-output $tagsupdate
+
+# Resource Group Information:
+
+$rgURI = $eventGridEvent.data.resourceUri
+write-output "rgURI:"
+write-output $rgURI
+Write-Host "Caller: $caller"
+Write-Host "ResourceId: $rgURI"
+
+if (($null -eq $caller) -or ($null -eq $rgURI)) {
     Write-Host "ResourceId or Caller is null"
     exit;
 }
@@ -22,21 +64,17 @@ if (($null -eq $caller) -or ($null -eq $resourceId)) {
 $ignore = @("providers/Microsoft.Resources/deployments", "providers/Microsoft.Resources/tags")
 
 foreach ($case in $ignore) {
-    if ($resourceId -match $case) {
+    if ($rgURI -match $case) {
         Write-Host "Skipping event as resourceId contains: $case"
         exit;
     }
 }
 
-$tags = (Get-AzTag -ResourceId $resourceId).Properties
+$tags = (Get-AzTag -ResourceId $rgURI).Properties
 
 if (!($tags.TagsProperty.ContainsKey('Creator')) -or ($null -eq $tags)) {
-    $tag = @{
-        Creator = $caller
-    }
-    Update-AzTag -ResourceId $resourceId -Operation Merge -Tag $tag
+    Update-AzTag -ResourceId $rgURI -Operation Merge -Tag $tagsupdate
     Write-Host "Added creator tag with user: $caller"
-}
-else {
+} else {
     Write-Host "Tag already exists"
 }
